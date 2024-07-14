70f1b82006f1f8707a200450e35a1ed79a8fd6ab
Dima Karpenko <dimka2010@gmail.com>
2024-07-14 13:52:33 +0300

    Concurrent connections #EJ5: async/await Server

--- src/Server.cs
+++ src/Server.cs
@@ -4,6 +4,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.IO;
 using System.Data;
+using System.Threading.Tasks;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -50,15 +51,74 @@ byte[] bytes = new byte[1024];
 int bytesRec;
 HttpRequest request = new HttpRequest();
 HttpResponse response;
-
+/*
+TcpClient
 while (true)
 {
-    var socket = server.AcceptSocket(); // wait for client    
+	TcpClient client = await server.AcceptTcpClientAsync();
+	_ = HandleClientAsync(client);
+}
 
-	while((bytesRec = socket.Receive(bytes)) != 0){
-		data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
-		request.ParseRequest(data);
-        response = router.Route(request);
-        socket.Send(Encoding.ASCII.GetBytes(response.GetFullResponse()));
-    }
+
+async Task HandleClientAsync(TcpClient client)
+{
+	try
+	{
+		using (NetworkStream stream = client.GetStream())
+		{
+			byte[] buffer = new byte[1024];
+			int bytesRead;
+
+			while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+			{
+				string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);					
+				request.ParseRequest(message);
+				response = router.Route(request);
+				var responseBytes = Encoding.ASCII.GetBytes(response.GetFullResponse());				
+				await stream.WriteAsync(responseBytes, 0, responseBytes.Length);
+			}
+		}
+	}
+	catch (Exception ex)
+	{
+		Console.WriteLine($"Error: {ex.Message}");
+	}
+	finally
+	{
+		client.Close();
+	}
+}
+*/
+/* Socket client */
+while (true)
+{
+		var socket = await server.AcceptSocketAsync(); // wait for client    
+		Console.WriteLine("Accept connect");
+		_= ProcessClientAsync(socket);
+}
+
+async Task ProcessClientAsync(Socket socket)
+{
+	string data = string.Empty;
+	byte[] bytes = new byte[1024];
+	int bytesRec;
+	try
+	{
+		while ((bytesRec = await socket.ReceiveAsync(new ArraySegment<byte>(bytes), SocketFlags.None)) != 0)
+		{
+			data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+			request.ParseRequest(data);
+			response = router.Route(request);
+			var msg = Encoding.ASCII.GetBytes(response.GetFullResponse());
+			//string message = "HTTP/1.1 200 OK\r\n\r\n";
+			//byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+
+			await socket.SendAsync(new ArraySegment<byte>(msg), SocketFlags.None);
+		}
+	}
+	finally
+	{
+		socket.Close();
+		socket.Dispose();
+	}
 }
